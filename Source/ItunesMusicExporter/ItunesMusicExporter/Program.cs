b32ae316cf08ed785378ced32572fdf88d31ecc0
Countryen <countryen@kabelbw.de>
2019-08-31 22:45:26 +0200

    Init .NET Core Project (Test)

---
+++ Source/ItunesMusicExporter/ItunesMusicExporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItunesMusicExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
